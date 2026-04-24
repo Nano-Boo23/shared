e4fe68a6d4830470e5ef8b24e8d961e0777a73ec
Nano_Boo <serra.a.gerard23@gmail.com>
2026-04-24 16:17:38 +0200

    initial commit

---
+++ docker_build_scripts/db-primaria/init.sql
@@ -0,0 +1,13 @@
+CREATE TABLE votos (
+    id TEXT PRIMARY KEY,
+    voto TEXT
+);
+
+INSERT INTO votos (id, voto)
+VALUES
+ ('test_id_1', 'Gatos'),
+ ('test_id_2', 'Gatos'),
+ ('test_id_3', 'Perros')
+;
+
+CREATE ROLE replicator WITH REPLICATION LOGIN PASSWORD 'replicator';
