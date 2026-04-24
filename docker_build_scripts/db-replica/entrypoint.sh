2cd8f241a6d1e394e488457b05f61ed9a3fc3d51
Nano_Boo <serra.a.gerard23@gmail.com>
2026-04-24 16:52:48 +0200

    Enhance entrypoint script for db replica setup
    Added a check for db-primaria readiness and cleaned previous data before starting the replica.

--- docker_build_scripts/db-replica/entrypoint.sh
+++ docker_build_scripts/db-replica/entrypoint.sh
@@ -2,13 +2,23 @@
 set -e
 
 echo "Esperando a db-primaria..."
-sleep 10
+until pg_isready -h db-primaria -p 5432 -U replicator; do
+  sleep 2
+done
 
+echo "Limpiando datos previos..."
 rm -rf /var/lib/postgresql/data/*
+
+echo "Cambiando permisos de postgresql/data..."
+chmod 700 /var/lib/postgresql/data
 
 export PGPASSWORD=replicator
 
+echo "Copiando base desde la primaria..."
 pg_basebackup -h db-primaria -D /var/lib/postgresql/data -U replicator -Fp -Xs -P -R
+
+echo "Asegurando permisos finales..."
+chmod 700 /var/lib/postgresql/data
 
 echo "Arrancando replica..."
 exec postgres
